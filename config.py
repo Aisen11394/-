4f6b30628b38f9f344d67e39875d6cdbe524e61c
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-04-27 20:18:35 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+token = '6681485877:AAEFYBnn-FyjC-kcehbw72mn2Vn_NWtmtoM' # Токен бота
+
+loginbot = '@Privatniyglazik_bot' # Логин бота телеграм
+
+
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+
+QIWI_TOKEN = 'xxxxxxxx' # токен киви
+
+QIWI_NUMBER = '77777777777' # Номер киви без +
+
+
+
+admin_id = 7119329476 # Ваш айди
+
+
+
+price = 190 # Цена за доступ
+
+
+
+#Ссылка на канал - @slivmenss
+
+#Наш чат - @slivmens_chat
