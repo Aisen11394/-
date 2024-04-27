c264f3cfecdc6bc917aabea8f1b06b21f976ec60
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-04-27 19:59:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,28 @@
-# First of all, you need to register and get a subscription to this api:
-# https://probivapi.com/
+import phonenumbers
+from phonenumbers import timezone, geocoder, carrier
+import telebot
 
-from aiogram import Bot, Dispatcher, executor, types
-import requests
-import json
+bot = telebot.TeleBot("token", parse_mode=None)
 
-# Telegram bot token
-API_TOKEN = 7187800106:AAE9qZR0Z632pRR0pPtnr1E8suC7cq0lvCw""
+@bot.message_handler(commands=["start"])
+def echo_welcome(message):
+    answer = "Привет я бот который сделает небольшой пробив\nпросто дай мне его номер"
+    bot.send_message(message.chat.id, answer)
 
-# ProbivAPI secret key
-PROBIVAPI_KEY = "24de6ae6-8bae-4445-8ee6-25367f6af35b"
+@bot.message_handler(content_types=["text"])
+def echo_num(message):
+    phonenumber = phonenumbers.parse(message.text)
+    carrier2 = carrier.name_for_number(phonenumber, "ru")
+    region = geocoder.description_for_number(phonenumber, "ru")
+    timezone1 = timezone.time_zones_for_number(phonenumber)
+    valid = phonenumbers.is_valid_number(phonenumber)
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+    answer0 = bot.send_message(message.chat.id, carrier2)
+    answer1 = bot.send_message(message.chat.id, timezone1)
+    answer2 = bot.send_message(message.chat.id, region)
+    if valid == True:
+        bot.send_message(message.chat.id, "Номер правельный")
+    elif valid == False:
+        bot.send_message(message.chat.id, "Номер не правильный")
 
-print("!BOT STARTED!")
-
-# Message handler that sends greeting when bot started
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Probiv Bot Template by DimonDev: @dimondevchat")
-
-
-# This is the main probiv function that returns a json and formats it, then sends it
-@dp.message_handler(content_types=['text'])
-async def text(message: types.Message):
-
-    # Get the message text and interpret it as a phone number
-    nomer = message.text
-    print(nomer)
-
-    # The endpoint for the probiv API that passes as a query the phone number
-    url = "https://probivapi.com/api/phone/info/" + nomer
-
-    # Necessary headers for the API to work
-    head = {
-        # API key that you can get by subscribing to the API
-        "X-Auth": PROBIVAPI_KEY
-    }
-
-    # Send the request with all the parameters and print the result for debugging
-    response = requests.get(url, headers=head)
-    print(response.text)
-
-    # Load the data of the response into a JSON object
-    try:
-        json_response = response.json()
-    except Exception:
-        json_response = {}
-
-    # Catch errors depending on the response
-    # Integrated TrueCaller API
-    try:
-        truecaller_api_name = str(json_response['truecaller']['data'][0]['name'])
-    except Exception:
-        truecaller_api_name = 'Not found'
-    # Integrated Numbuster API
-    try:
-        numbuster_api_name = str(json_response['numbuster']['averageProfile']['firstName']) + \
-        str(json_response['numbuster']['averageProfile']['lastName'])
-    except Exception:
-        numbuster_api_name = 'Not found'
-    # Integrated EyeCon API
-    try:
-        eyecon_api_name = str(json_response['eyecon'])
-    except Exception:
-        eyecon_api_name = 'Not found'
-    # Integrated ViewCaller API
-    try:
-        viewcaller_name_list = []
-        for tag in json_response['viewcaller']:
-            viewcaller_name_list.append(tag['name'])
-        viewcaller_api_name = ', '.join(viewcaller_name_list)
-    except Exception:
-        viewcaller_api_name = 'Not found'
-
-    # Send the formatted data to the user on Telegram
-    await bot.send_message(message.chat.id,f"""
-                           
-                           📱 ФИО (Numbuster): {numbuster_api_name}
-                           🌐 ФИО (EyeCon): {eyecon_api_name}
-                           🔎 ФИО (ViewCaller): {viewcaller_api_name}
-                           📞 ФИО (TrueCaller): {truecaller_api_name}
-
-                           @dimondev_ru
-                           
-                           Код бота: https://github.com/SegYT/glazboga/
-
-                           Пробив API: https://probivapi.com
-                           """)
-
-
-# Main loop
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+bot.infinity_polling()
